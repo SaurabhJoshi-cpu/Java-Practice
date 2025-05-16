a4bfcb5234f276927389c3548702b5c21d47813d
Saurabh Joshi <joshisaurabh2006@gmail.com>
2025-05-16 13:59:26 +0530

    Add files via upload

---
+++ Java-Practice-main/Java-Practice-main/Functions practice/funcprac1/src/Main.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        String gugu=greetings();
+        System.out.println(gugu);
+    }
+    //return a string or print a message
+    static String greetings(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your greetings");
+        String gree=sc.nextLine();
+        return gree;
+
+    }
+}
